bbb8cb2001ed0ff93d7024154269afe47d891c8a
henrylu <luxm@163.com>
2025-07-04 16:43:15 +0800

    Update FabricConnection.java

--- src/main/java/com/webank/wecross/stub/fabric2/FabricConnection.java
+++ src/main/java/com/webank/wecross/stub/fabric2/FabricConnection.java
@@ -288,7 +288,7 @@ public class FabricConnection implements Connection {
                 return FabricConnectionResponse.build()
                         .errorCode(FabricType.TransactionResponseStatus.SUCCESS)
                         .errorMessage(response.getMessage())
-                        .data(objectMapper.writeValueAsBytes(response.getSubscribeId()));
+                        .data(response.getSubscribeId().getBytes(StandardCharsets.UTF_8));
             }
         } catch (Exception e) {
             return FabricConnectionResponse.build()
